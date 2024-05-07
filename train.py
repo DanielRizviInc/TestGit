6bebde2aaa92767105ec1871b82fcea164f823d7
Daniel Rizvi <danielrizvict@gmail.com>
2024-05-07 12:55:16 +0530

    Added Features

--- train.py
+++ train.py
@@ -3,7 +3,10 @@ from ultralytics import YOLO
 # Load a model
 # model = YOLO('yolov8n.yaml')  # build a new model from YAML
 # model = YOLO('yolov8m.pt')  # load a pretrained model (recommended for training)
-model = YOLO('yolov8m.yaml').load('yolov8x.pt')  # build from YAML and transfer weights
+model = YOLO('yolov8m1.yaml').load('yolov8x.pt')  # build from YAML and transfer weights
 
 # Train the model
 results = model.train(data='sanitize.yaml', epochs=25, imgsz=640)
+
+print("Daniel Rizvi Updated Text")
+print("New Text!!!")
